ba1025fff0af37cdf2707dea8f565ed271f1b39c
Peristratus <testcorp24@gmail.com>
2020-12-26 22:54:55 +0000

    adding the layout div's for the shopping cart

--- payment.php
+++ payment.php
@@ -7,7 +7,40 @@
     <link rel="stylesheet" href="#">
 </head>
 <body>
-    
+    <main id= "cart-main">
+       <div class ="site-title text-center">
+           <h1 class= "font-title">Shopping Cart</h1>
+       </div>
+
+       <div class="container">
+           <div class="grid">
+               <div class ="flex item justify-content-between">
+                   <div class="flex">
+                       <div class ="img text-center">
+                           <img src= "../img/star22.jpg" alt ="Atari star Wars game">
+                       </div>
+                       <div class= "title">
+                           <h3>Star Wars</h3>
+                           <span>Atari Game-Arcade Version</span>
+
+                           <div class= "buttons">
+                               <button  type = "submit"><i class= "fas fa-chevron-up"></i></button>
+                               <input   type = "text" class = "font-title" value = "1">
+                               <button  type = "submit"><i class= "fas fa-chevron-down"></i></button> 
+                           </div>
+                           <a href ="#">Save for later</a>
+                           <a href ="#">Delete from Cart</a>
+                       </div>
+                   </div>
+                   <div class ="price">
+                     <h4 class= "text-red">$59.78</h4>
+                   </div>
+               </div>
+            </div>
+
+
+
+    </main>
 
 
 
