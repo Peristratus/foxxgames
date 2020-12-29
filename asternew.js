65454d46cce606efc58aee1cd676dc174efe1490
Peristratus <testcorp24@gmail.com>
2020-12-29 20:31:03 +0000

    added the code for detecting collision on enemy hit player

--- asternew.js
+++ asternew.js
@@ -100,9 +100,9 @@ function spawnEnemies() {
     }, 1000)
 
 }
-
+let animationId
 function animate() {
-   requestAnimationFrame(animate)
+   animationId = requestAnimationFrame(animate)
    ctx.clearRect(0, 0, canvas.width, canvas.height)
    player.draw()
    projectiles.forEach((projectile)=> {
@@ -110,6 +110,12 @@ function animate() {
    })
    enemies.forEach((enemy, index) => {
      enemy.update()
+
+     const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+      // end game
+      if (dist - enemy.radius - player.radius < 1 ){
+          cancelAnimationFrame(animationId)
+      }
 
        projectiles.forEach((projectile, projectileIndex) => {
          const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
