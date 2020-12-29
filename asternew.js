9dd46442c23e51b70b88a3278521bd5a22b7f25f
Peristratus <testcorp24@gmail.com>
2020-12-29 02:58:57 +0000

    code for the projectiles on x and y axis angle = atan2 to calculate each click on event

--- asternew.js
+++ asternew.js
@@ -34,6 +34,12 @@ class Projectile{
    ctx.fillStyle = this.color
    ctx.fill()
   }
+  update() {
+   this.draw()
+   this.x= this.x + this.velocity.x
+   this.y= this.y + this.velocity.y
+
+  }
 
 }
 const x = canvas.width /2
@@ -41,9 +47,31 @@ const y = canvas.height/2
 
 const player = new Player (x, y, 30, 'blue')
 player.draw()
+ 
+const projectiles = []
+
+function animate() {
+   requestAnimationFrame(animate)
+   projectiles.forEach((projectile)=> {
+   projectile.update()
+
+   })
+}
 
 addEventListener('click', (event) => 
 {
-  const projectile = new Projectile(  event.clientX, event.clientY, 5,"red", null)
-    projectile.draw()
+  const angle = Math.atan2(
+      event.clientY - canvas.height / 2, 
+      event.clientX - canvas.width / 2)
+    
+  projectiles.push(new Projectile(
+      canvas.width /2, canvas.height /2, 
+      5, 'red', 
+      {x: 1, 
+       y: 1
+      })
+    )
+    
 })
+
+animate()
