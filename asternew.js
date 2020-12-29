6175dcadd94fb3ea54252b2ec134d53e809b3784
Peristratus <testcorp24@gmail.com>
2020-12-29 01:32:37 +0000

    adding the code for the projectiles to shoot from the player in the center

--- asternew.js
+++ asternew.js
@@ -3,6 +3,7 @@ const ctx =canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+//Draw the palyer
 class Player{
   constructor(x, y, radius, color){
     this.x = x
@@ -15,11 +16,34 @@ class Player{
    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
    ctx.fillStyle = this.color
    ctx.fill()
+  }
+}
 
+class Projectile{
+  constructor(x, y, radius, color, velocity) {
+   this.x = x
+   this.y = y
+   this.radius = radius
+   this.color = color
+   this.velocity = velocity
+
+  }
+   draw() {
+   ctx.beginPath()
+   ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+   ctx.fillStyle = this.color
+   ctx.fill()
   }
 
 }
+const x = canvas.width /2
+const y = canvas.height/2
 
-const player = new Player (100, 100, 30, 'blue')
+const player = new Player (x, y, 30, 'blue')
 player.draw()
-console.log(player)
+
+addEventListener('click', (event) => 
+{
+  const projectile = new Projectile(  event.clientX, event.clientY, 5,"red", null)
+    projectile.draw()
+})
