108d064194650a5337ef5f840217f1bc37b6c062
Peristratus <testcorp24@gmail.com>
2020-12-31 03:17:05 +0000

    testing the neural matrix

--- neural_network.js
+++ neural_network.js
@@ -42,8 +42,77 @@ class Matrix {
 
         return this._data;
     }
-      // apply randon weights betw -1 & 1
 
+      // add two matrices 
+      static add(m0, m1) {
+            Matrix.checkDimensions(m0, m1)
+            let m = new Matrix(m0.rows, m0.cols);
+            for (let i = 0; i < m.rows; i ++){
+                for (let j = 0; j < m.cols; j++) {
+                     m.data[i][j] = m0.data[i][j] + m1.data[i][j];
+                }
+       
+            }
+              return m;
+        }  
+
+       // check matrices have the same dimensions
+
+       static checkDimensions(m0, m1) {
+          if (m0.rows != m1.rows || m0.cols != m1.cols){
+
+            throw new Error ("Matrices of different dimentions!")
+          }
+
+       }
+
+       //dot product of two matrices
+        static dot(m0, m1){
+            if(m0.col != m1.rows){
+                throw new Error("Matrices are not \"dot\"compatiable");
+            }
+
+            let m = new Matrix(m0.rows, m1.cols);
+            for (let i = 0; i < m.rows; i ++){
+                for (let j = 0; j < m.cols; j++) {
+                     let sum = 0;
+                     for (let k = 0; k < m0.cols; k++){
+                          
+
+                     }
+                }
+       
+            }
+        }
+
+       // multiplying two matrices (not the dot product)
+      static multiply(m0, m1) {
+            Matrix.checkDimensions(m0, m1)
+            let m = new Matrix(m0.rows, m0.cols);
+            for (let i = 0; i < m.rows; i ++){
+                for (let j = 0; j < m.cols; j++) {
+                     m.data[i][j] = m0.data[i][j] * m1.data[i][j];
+                }
+       
+            }
+              return m;
+        }  
+
+    // subtract two matrices 
+      static subtract(m0, m1) {
+            Matrix.checkDimensions(m0, m1)
+            let m = new Matrix(m0.rows, m0.cols);
+            for (let i = 0; i < m.rows; i ++){
+                for (let j = 0; j < m.cols; j++) {
+                     m.data[i][j] = m0.data[i][j] - m1.data[i][j];
+                }
+       
+            }
+              return m;
+        }  
+
+
+      // apply randon weights betw -1 & 1
     randomWeights(){
 
         for (let i = 0; i < this.rows; i++){
