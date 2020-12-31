2ae26f0f4e2927a3689db3ca81fd9c80374d1846
Peristratus <testcorp24@gmail.com>
2020-12-31 02:10:44 +0000

    setting up the code for the neural weights

--- neural_network.js
+++ neural_network.js
@@ -9,6 +9,23 @@ class Matrix {
       this._data= data;
 
       // initialize with zeros if no data provided
+
+      if(data == null || data.length == 0){
+
+        this._data = [];
+        for (let i = 0; i < this._rows; i++){
+            this.data[i]=[];
+            for (let j = 0; j < this._cols; j++){
+                this._data[i][j] = 0;
+
+            }
+        }
+      } else {
+          //check data integrity
+          if(data.length != rows || data[0].length != cols){
+              throw new Error("Incorrect data dimensions!")
+          }
+      }
    }
    
     get rows() {
@@ -25,5 +42,15 @@ class Matrix {
 
         return this._data;
     }
+      // apply randon weights betw -1 & 1
 
+    randomWeights(){
+
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.cols; j++) {
+                this.data[i][j] = Math.random()* 2 -1;
+            }
+
+        }
+    }
 }
